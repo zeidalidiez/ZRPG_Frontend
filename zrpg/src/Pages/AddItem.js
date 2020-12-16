abeede46a28615544d8a72fc032da14a7d3c9a57
zeidalidiez <zeidalidiez@gmail.com>
2020-12-16 12:43:26 -0800

    AddItem

--- zrpg/src/Pages/AddItem.js
+++ zrpg/src/Pages/AddItem.js
@@ -1,9 +1,35 @@
 
+import React, { Component } from  'react';
 
-function AddItem() {
-  return (
-<h2> Add Item Function</h2>
-  );
+
+class AddItem extends Component {
+  render() {
+    return (
+      <div className="additem">
+        <h2>Add an item</h2>
+        <form className="additem__form">
+          <label htmlFor="title">Title:</label>
+           <input type="text" name="title" id="title" placeholder="Title"/>
+          <label htmlFor="url">Url:</label>
+           <input type="text" name="url" id="url" placeholder="url"/>
+          <label htmlFor="description">Description:</label>
+            <textarea name="description" id="description" placeholder="description"/>
+          <label htmlFor="rating">Rating: </label>
+              <input
+              type="number"
+              name="rating"
+              id="rating"
+              min="1"
+              max="5"/>
+
+          <div className="addbookmark__buttons">
+            <button>Cancel</button>
+            <button type="submit" >Save</button>
+          </div>  
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddItem;
